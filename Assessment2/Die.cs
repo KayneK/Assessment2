7d86de13a8695ffdfafa1341c1cb0359318b3da9
KayneK <43245746+KayneK@users.noreply.github.com>
2024-04-23 12:14:17 +0100

    Added files for classes, implemented Die class

---
+++ Assessment2/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    internal class Die
+    {
+
+        public Die() {
+            // Create a new instance of the Random class
+            Random rnd = new Random();
+
+            // Generate a random number between 1 and 6
+            Roll = rnd.Next(1, 7);
+        }
+
+        public int Roll;
+    }
+}
---
+++ Assessment2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die = new Die();
+            Console.WriteLine(die.Roll);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
